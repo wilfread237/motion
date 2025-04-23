ec9e97a2289f93ccf902806ad5188f97aec8c213
Wilfread Essomba <wilfreadessomba@gmail.com>
2025-04-23 16:11:12 +0100

    initializing of master branch

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,11 @@
 import { useState } from 'react';
 import './App.css'
-import Cards from './Components/Cards';
 
 function App() {
-  const [card, setCards] = useState();
 
   return (
     <div className='flex flex-col justify-center items-center min-h-screen'>
-     <p className='text-xl font-medium font-mono'>Components with Motion</p>
-     <Cards/>
+     <p className='text-xl font-medium font-mono'>Hello we</p>
     </div>
   )
 }
